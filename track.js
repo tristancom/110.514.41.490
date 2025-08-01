c1fa57b01a0ee8ee647167a823c7b98ff93c71b1
tristancom <ecaasapi@gmail.com>
2025-08-01 21:47:12 +0700

    Update track.js

--- track.js
+++ track.js
@@ -2,10 +2,17 @@ function trackIP() {
   fetch('https://api.ipify.org?format=json')
     .then(res => res.json())
     .then(data => {
-      // Kirim data ke Google Apps Script
-      fetch(`https://script.google.com/macros/s/AKfycbxfW9GSweTyvFiMtvM4mC8rAlD2Je6xPO7XGpOo_88Hz-pP-cjudu9W5fjqysHjs2EU9g/exec?ip=${data.ip}&ua=${encodeURIComponent(navigator.userAgent)}`);
+      const ip = data.ip;
+      const ua = navigator.userAgent;
       
-      // Redirect pengunjung (opsional)
-      window.location.href = "https://example.com/download";
+      // Kirim data ke Apps Script (ganti YOUR_SCRIPT_ID)
+      fetch(`https://script.google.com/macros/s/AKfycbwEjUViowX-2Wia2UBp_RzI6eF5XwFuj0ovNWnMxwzSiRYK8JKjWOsiKPbEPDz-X6ymeg/exec?ip=${ip}&ua=${encodeURIComponent(ua)}`, {
+        redirect: 'follow',
+        mode: 'no-cors'
+      })
+      .then(() => {
+        window.location.href = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQjPdejqvFg4zR-nhOBS5sRFZwlh4MN7J97S4z1KrBSiSjNtbft2uF_j0HOxpX3UlFgfXIikOWctTC_/pubhtml?gid=0&single=true"; // Redirect setelah kirim data
+      })
+      .catch(err => console.error("Error:", err));
     });
 }
